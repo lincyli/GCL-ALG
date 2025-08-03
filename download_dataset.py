f9c70206fa963d5d61637557c3b254a86303f463
lincyli <100744783+lincyli@users.noreply.github.com>
2025-08-03 15:55:12 +0800

    Add files via upload

---
+++ download_dataset.py
@@ -0,0 +1,15 @@
+from torchvision.datasets import MNIST
+from torch_geometric.datasets import MNISTSuperpixels
+from datasets import get_dataset
+
+dataset_list = ['MUTAG',
+    'NCI1',
+    'PROTEINS',
+    'DD',
+    'COLLAB',
+    'IMDB-BINARY',
+    'REDDIT-MULTI-5K']
+
+for dataset_name in dataset_list:
+    dataset = get_dataset(dataset_name, sparse=True, feat_str='deg+odeg100', root='data')
+    print(dataset)
